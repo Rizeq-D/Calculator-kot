00bbadfab1f6713f463b6966cf849e43ccc21187
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-08-09 12:21:46 +0200

    added new buttons to the calc

--- app/src/main/java/com/example/calculator_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculator_kotlin/MainActivity.kt
@@ -1,26 +1,49 @@
 package com.example.calculator_kotlin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.calculator_kotlin.ui.theme.CalculatorkotlinTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var display : TextView
+    private var currentInput : String = ""
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CalculatorkotlinTheme {
+        setContentView(R.layout.activity_main)
 
-            }
-        }
+        display = findViewById(R.id.display)
+
+        val buttonsId = listOf(
+            R.id.btnOne, R.id.btnTwo, R.id.btnThree, R.id.btnDivide
+        )
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
