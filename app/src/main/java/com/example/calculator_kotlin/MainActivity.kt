ce4f3989731d43bbd652c7ec7d7383a425374484
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-08-09 14:11:48 +0200

    added more buttons, functionalities, and sat up the expression evaluation of the calc

--- app/src/main/java/com/example/calculator_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculator_kotlin/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : ComponentActivity() {
 
@@ -19,10 +20,60 @@ class MainActivity : ComponentActivity() {
         display = findViewById(R.id.display)
 
         val buttonsId = listOf(
-            R.id.btnOne, R.id.btnTwo, R.id.btnThree, R.id.btnDivide
+            R.id.btnOne, R.id.btnTwo, R.id.btnThree,
+            R.id.btnFour, R.id.btnFive, R.id.btnSix,
+            R.id.btnSeven, R.id.btnEight, R.id.btnNine,
+            R.id.btnZero
         )
+        for (id in buttonsId) {
+            findViewById<Button>(id).setOnClickListener {
+                button -> val buttonText = (button as Button).text.toString()
+                currentInput += buttonText
+                display.text = currentInput
+            }
+        }
 
+        val operatorButtonsId = listOf(
+            R.id.btnPlus, R.id.btnDivide,
+            R.id.btnSubtract, R.id.btnMultiple
+        )
+        for (id in operatorButtonsId) {
+            findViewById<Button>(id).setOnClickListener { button ->
+                val buttonText = (button as Button).text.toString()
+                currentInput += buttonText
+                display.text = currentInput
+            }
+        }
 
+        findViewById<Button>(R.id.btnClear).setOnClickListener {
+            currentInput = ""
+            display.text = currentInput
+        }
+        findViewById<Button>(R.id.btnDecimalDot).setOnClickListener {
+            currentInput += "."
+            display.text = currentInput
+        }
+        findViewById<Button>(R.id.btnEquals).setOnClickListener {
+            calculateResults()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun calculateResults() {
+
+        try {
+            val result = evaluate(currentInput)
+            display.text = result.toString()
+            currentInput = result.toString()
+
+        }catch(e : Exception){
+            display.text = "Error"
+            currentInput = ""
+        }
+    }
+
+    private fun evaluate(expr: String): Double {
+        return ExpressionBuilder(expr).build().evaluate()
     }
 }
 
